c1d1312107276b0c3193953a6d48c7646853b3d6
TM <tonimaxx@gmail.com>
2021-09-15 14:06:46 -0700

    Update product search test to iPhone13

--- sample_script.py
+++ sample_script.py
@@ -11,7 +11,7 @@ driver.get('https://www.google.com/')
 
 search = driver.find_element(By.NAME, 'q')
 search.clear()
-search.send_keys('Dress')
+search.send_keys('iphone13')
 
 # wait for 4 sec
 sleep(4)
@@ -20,7 +20,7 @@ sleep(4)
 driver.find_element(By.NAME, 'btnK').click()
 
 # verify
-assert 'dress' in driver.current_url.lower(), f"Expected query not in {driver.current_url.lower()}"
+assert 'iphone13' in driver.current_url.lower(), f"Expected query not in {driver.current_url.lower()}"
 print('Test Passed')
 
 driver.quit()
